2983d3ace38229ea78ef6b00f14f8cd5ea822bc2
shiradahan150 <shiradahan150@gmail.com>
2025-11-26 21:52:54 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,14 @@
 def create_codon_dict(file_path):
-hghg
+  dict_codon_to_amino = {}
+
+  with open(file_path, 'r') as file:
+    rows = file.readlines()
+    for row in rows:
+      row_cells = row.strip().split('\t')
+      codon = row_cells[0]
+      amino_acid = row_cells[2]
+      dict_codon_to_amino[codon] = amino_acid
+  
+  return dict_codon_to_amino
 
 
